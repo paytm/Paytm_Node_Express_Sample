02adcdbbcb22f46ef9f2b746ec74f4f8bd5c73d1
vidisha-goel <vidisha.goel@paytm.com>
2016-03-14 19:25:50 +0530

    Plugin Submit

---
+++ config/config.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var PAYTM_STAG_URL = 'https://pguat.paytm.com';
+var PAYTM_PROD_URL = 'https://secure.paytm.in';
+var MID = 'WorldP64425807474247';
+var PAYTM_ENVIORMENT = 'TEST';   // PROD FOR PRODUCTION
+var PAYTM_MERCHANT_KEY = 'kbzk1DSbJiV_O3p5';
+var WEBSITE = 'worldpressplg';
+var CHANNEL_ID =  'WEB';
+var INDUSTRY_TYPE_ID = 'Retail';
+var PAYTM_FINAL_URL = '';
+if (PAYTM_ENVIORMENT== 'TEST') {
+
+ PAYTM_FINAL_URL = PAYTM_STAG_URL + '/oltp-web/processTransaction'
+}else
+{
+  PAYTM_FINAL_URL = PAYTM_PROD_URL + '/oltp-web/processTransaction'
+}
+
+module.exports = {
+
+  
+  //paytmnew: {
+    MID: MID,
+    PAYTM_MERCHANT_KEY :PAYTM_MERCHANT_KEY,
+    PAYTM_FINAL_URL :PAYTM_FINAL_URL,
+    WEBSITE: WEBSITE,
+    CHANNEL_ID: CHANNEL_ID,
+    INDUSTRY_TYPE_ID: INDUSTRY_TYPE_ID
+ // }
+};
---
+++ routes/admin/pgredirect.js
@@ -0,0 +1,10 @@
+var checksum = require('../../model/checksum');
+
+module.exports = function (app) {
+
+	app.get('/pgredirect', function(req,res){
+   console.log("in pgdirect");
+console.log("--------testtxnjs----");
+res.render('pgredirect.ejs');
+  });
+};
